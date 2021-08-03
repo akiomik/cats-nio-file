593ab51b75fc14dcd8d99f75774a20964d2ae76e
Akiomi Kamakura <akiomik@gmail.com>
2021-08-03 16:47:54 +0900

    Bump cats-effect version to 3.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val scalaJava8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.0"
-  lazy val catsEffect       = "org.typelevel"          %% "cats-effect"        % "3.0.2"
+  lazy val catsEffect       = "org.typelevel"          %% "cats-effect"        % "3.1.1"
   lazy val scalaTest        = "org.scalatest"          %% "scalatest"          % "3.2.9"
 }
